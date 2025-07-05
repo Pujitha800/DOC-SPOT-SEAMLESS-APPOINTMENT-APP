0eb65a8e85a4ba74d6af86e58833ddb0a375bc0d
Pujitha800 <224g1a3272@srit.ac.in>
2025-07-05 20:40:45 +0530

    Update userController.js

--- DocSpot_FullStructure/backend/controllers/userController.js
+++ DocSpot_FullStructure/backend/controllers/userController.js
@@ -1,1 +1,22 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.create({ name, email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
+  if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET);
+  res.json({ token, user });
+};
 // userController.js - Placeholder content
