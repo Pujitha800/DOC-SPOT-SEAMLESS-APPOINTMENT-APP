f7a78566fd12a28a2942db13d40db24b93b790fb
Pujitha800 <224g1a3272@srit.ac.in>
2025-07-05 20:26:51 +0530

    Update mongodb.js

--- DocSpot_FullStructure/backend/config/mongodb.js
+++ DocSpot_FullStructure/backend/config/mongodb.js
@@ -1,11 +1,15 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(${process.env.MONGODB_URI}/prescripto)
+module.exports = connectDB;
 
-}
-
-export default connectDB;
 // mongodb.js - Placeholder content
