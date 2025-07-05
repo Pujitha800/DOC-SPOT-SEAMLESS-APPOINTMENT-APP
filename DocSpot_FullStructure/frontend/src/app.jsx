5c07f666b7b600aad8362789b7f8c266de312289
Pujitha800 <224g1a3272@srit.ac.in>
2025-07-05 20:42:29 +0530

    Update app.jsx

--- DocSpot_FullStructure/frontend/src/app.jsx
+++ DocSpot_FullStructure/frontend/src/app.jsx
@@ -1,1 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 // app.jsx - Placeholder content
