a73680025f15eaa3a6dca14b1ea7e64f45fc080a
Pujitha800 <224g1a3272@srit.ac.in>
2025-07-05 21:30:26 +0530

    Update vite.config.js

--- DocSpot_FullStructure/frontend/vite.config.js
+++ DocSpot_FullStructure/frontend/vite.config.js
@@ -1,1 +1,8 @@
-// vite.config.js - Placeholder content
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{port:5173}
+})// vite.config.js - Placeholder content
